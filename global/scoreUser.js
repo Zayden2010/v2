cf26880c7fac7882aa5a7dd2182bb0c31fcd7ab5
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-11-23 11:42:54 -0500

    remapped

--- global/scoreUser.js
+++ global/scoreUser.js
@@ -1,24 +1,36 @@
-async function score(t) {
-    let w = (await (await fetch(`https://v2.blacket.org/worker/user/${t}`)).json());
-      if (w.error) return alert(w.error);
-      let e = w.user,
-      r = e.blooks,
-      a = (await (await fetch("https://v2.blacket.org/worker/blooks")).json()).blooks,
-      o = Object.keys(a),
-      s = o.length,
-      i = Object.keys(r).length / s,
-      k = Object.keys(r).map(t => {
-        if (!a[t]) return;
-        let e = a[t];
-        return e._name = t, e
-      }).filter(Boolean).filter(t => "Blacket" != t._name),
-      l = Object.keys((await (await fetch("https://v2.blacket.org/worker/rarities")).json()).rarities),
-      n = 0;
-    for (let w of k) n += l.indexOf(w.rarity) + 1;
-    return {
-      blooks: Number((100 * i).toFixed(2)),
-      rarityScore: Number(n.toFixed(2))
-    }
+'use strict';
+/**
+ * @param {string} appears
+ * @return {?}
+ */
+async function score(appears) {
+  let jsonRpc = await (await fetch(`https://v2.blacket.org/worker/user/${appears}`)).json();
+  if (jsonRpc.error) {
+    return alert(jsonRpc.error);
   }
-  
-  score(prompt('Enter a user\'s name to score...\Created by l2vy7'));
+  let e = jsonRpc.user;
+  let r = e.blooks;
+  let equalizerPresets = (await (await fetch("https://v2.blacket.org/worker/blooks")).json()).blooks;
+  let expRecords = Object.keys(equalizerPresets);
+  let idealMessageSize = expRecords.length;
+  let labelPointScale = Object.keys(r).length / idealMessageSize;
+  let entries = Object.keys(r).map((name) => {
+    if (!equalizerPresets[name]) {
+      return;
+    }
+    let obj = equalizerPresets[name];
+    return obj._name = name, obj;
+  }).filter(Boolean).filter((t) => {
+    return "Blacket" != t._name;
+  });
+  let loadList = Object.keys((await (await fetch("https://v2.blacket.org/worker/rarities")).json()).rarities);
+  let e_total = 0;
+  for (let entry of entries) {
+    e_total = e_total + (loadList.indexOf(entry.rarity) + 1);
+  }
+  return {
+    blooks : Number((100 * labelPointScale).toFixed(2)),
+    rarityScore : Number(e_total.toFixed(2))
+  };
+}
+score(prompt("Enter a user's name to score...Created by l2vy7"));
