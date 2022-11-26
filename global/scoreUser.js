413196e172d0b89a4189105ae452e70ab904fc42
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-11-25 22:38:51 -0500

    works with async/await properly

--- global/scoreUser.js
+++ global/scoreUser.js
@@ -1,13 +1,6 @@
-'use strict';
-/**
- * @param {string} appears
- * @return {?}
- */
 async function score(appears) {
   let jsonRpc = await (await fetch(`https://v2.blacket.org/worker/user/${appears}`)).json();
-  if (jsonRpc.error) {
-    return alert(jsonRpc.error);
-  }
+  if (jsonRpc.error) return alert(jsonRpc.error);
   let e = jsonRpc.user;
   let r = e.blooks;
   let equalizerPresets = (await (await fetch("https://v2.blacket.org/worker/blooks")).json()).blooks;
@@ -15,9 +8,7 @@ async function score(appears) {
   let idealMessageSize = expRecords.length;
   let labelPointScale = Object.keys(r).length / idealMessageSize;
   let entries = Object.keys(r).map((name) => {
-    if (!equalizerPresets[name]) {
-      return;
-    }
+    if (!equalizerPresets[name]) return;
     let obj = equalizerPresets[name];
     return obj._name = name, obj;
   }).filter(Boolean).filter((t) => {
@@ -25,12 +16,7 @@ async function score(appears) {
   });
   let loadList = Object.keys((await (await fetch("https://v2.blacket.org/worker/rarities")).json()).rarities);
   let e_total = 0;
-  for (let entry of entries) {
-    e_total = e_total + (loadList.indexOf(entry.rarity) + 1);
-  }
-  return {
-    blooks : Number((100 * labelPointScale).toFixed(2)),
-    rarityScore : Number(e_total.toFixed(2))
-  };
-}
-score(prompt("Enter a user's name to score...Created by l2vy7"));
+  for (let entry of entries) e_total = e_total + (loadList.indexOf(entry.rarity) + 1);
+  alert(`${appears}'s score is ${Number((100 * labelPointScale).toFixed(2))}`);
+};
+score(prompt(`Enter username to score...`));
