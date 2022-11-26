eae7490880ed85d13b8b7c092f35153e9d0625b5
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-11-25 22:55:13 -0500

    better

--- blooks/sellDupes.js
+++ blooks/sellDupes.js
@@ -1,17 +1,27 @@
-let qnt;
+if (!confirm('Do you actually want to sell all of your duplicate blooks?')) alert(`I'm glad I asked.`);
+else {
+  if (!blacket.blooks) {
+    alert(`You must be on the market or blooks page to run this script.`);
+    location.reload();
+  }
+  
+  let qnt;
 
-function sellBlook(blook, amount) {
-  blacket.requests.post("/worker/sell", {
-    blook: blook,
-    quantity: amount
-  }, (res) => {});
-};
-
-Object.keys(blacket.blooks).forEach((blook) => {
-  if (Object.keys(blacket.user.blooks).includes(blook)) {
-    qnt = blacket.user.blooks[blook] - 1;
-    if (qnt == 0) return;
-    sellBlook(blook, qnt);
-    console.log(`Selling ${qnt} ${blook}`);
+  function sell(blook, amount) {
+    blacket.requests.post("/worker/sell", {
+      blook: blook,
+      quantity: amount
+    }, (res) => {
+      if (res.error) location.reload();
+    });
   };
-});
+
+  Object.keys(blacket.blooks).forEach((blook) => {
+    if (Object.keys(blacket.user.blooks).includes(blook)) {
+      qnt = blacket.user.blooks[blook] - 1;
+      if (qnt == 0) return;
+      sell(blook, qnt);
+      console.log(`Selling ${qnt} ${blook}`);
+    };
+  });
+}
